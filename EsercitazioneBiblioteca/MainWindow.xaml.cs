5476d05f65ecf18932b9c86a884e05297e21a4dc
Sara Cappelli <SARA.CAPPELLI@local.ittcesena.it>
2022-11-17 09:10:38 +0100

    Interfaccia 1.0

--- EsercitazioneBiblioteca/MainWindow.xaml.cs
+++ EsercitazioneBiblioteca/MainWindow.xaml.cs
@@ -24,5 +24,85 @@ namespace EsercitazioneBiblioteca
         {
             InitializeComponent();
         }
+
+        private void aggiungiLibro_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ControllaCampiAggiuntaLibro())
+            {
+                Biblioteca.AggiungiLibro(new Libro(titolo_input.Text, autore_input.Text, int.Parse(annoPbl_input.Text), editore_input.Text, int.Parse(numeroPagine_input.Text)));
+            }
+            ReimpostaCampiAggiuntaLibro();
+        }
+
+        bool ControllaCampiAggiuntaLibro()
+        {
+            // TODO
+        }
+
+        void ReimpostaCampiAggiuntaLibro()
+        {
+            titolo_input.Text = "";
+            autore_input.Text = "";
+            annoPbl_input.Text = "";
+            editore_input.Text = "";
+            numeroPagine_input.Text = "";
+        }
+
+
+        #region Filtri
+        TipoDiFiltro filtroAttuale;
+        private void tipoFiltro_button_Click(object sender, RoutedEventArgs e)
+        {
+            int num = (int)filtroAttuale;
+            num++;
+            
+            if (!Enum.IsDefined(typeof(TipoDiFiltro), num))
+            {
+                num = 0;
+            }
+            filtroAttuale = (TipoDiFiltro)num;
+            tipoFiltro_button.Content = filtroAttuale;
+        }
+
+        enum TipoDiFiltro
+        {
+            Titolo,
+            Autore
+        }
+
+        private void aggiungiFiltro_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (filtroAttuale == TipoDiFiltro.Titolo)
+            {
+                AggiornaListaLibri(Biblioteca.FiltraPerTitolo(filtro_input.Text));
+            }
+            else if (filtroAttuale == TipoDiFiltro.Autore)
+            {
+                AggiornaListaLibri(Biblioteca.FiltraPerAutore(filtro_input.Text));
+            }
+        }
+
+
+        #endregion
+
+        void AggiornaListaLibri(List<Libro> lista)
+        {
+            listaLibri_listBox.Items.Clear();
+            foreach (Libro l in lista)
+            {
+                listaLibri_listBox.Items.Add(l);
+            }
+        }
+
+        private void eliminaFiltri_button_Click(object sender, RoutedEventArgs e)
+        {
+            AggiornaListaLibri(Biblioteca.ListaLibri);
+        }
+
+        private void listaLibri_listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            libroSelInfo_txtBlock.Text = listaLibri_listBox.SelectedItem.toString();
+            tempoLettura_txtBlock.Text = listaLibri_listBox.SelectedItem.ReadingTime();
+        }
     }
 }
